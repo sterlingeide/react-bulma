a7bf72c7f435316264dfdedcd0c8cdd180468795
sterlingeide <sterlinga.eide@gmail.com>
2021-12-16 11:43:50 -0800

    added comment code to forum

--- src/Forum.js
+++ src/Forum.js
@@ -1,6 +1,60 @@
 import React, { Component } from 'react';
 import './Forum.css';
 
+const comment = {
+    message: 'I love coding. All day long',
+    author: '@SterlingEide'
+}
+
+const commentList = [
+    {
+        message: 'Coding is aight',
+        author: '@yourBoy'
+    },
+    {
+        message: 'Coding is a things',
+        author: '@Timothy'
+    },
+    {
+        message: 'Time to code',
+        author: '@Mike'
+    },
+    {
+        message: 'Coding, all day long.',
+        author: '@Barthalamule'
+    },
+    {
+        message: 'Code is neverending',
+        author: '@Jebediah'
+    },
+]
+
+
+const displayCommentList = commentList.map((c, idx) => {
+    return (
+        <article className="post" key={idx} >
+            <h4>{c.message}</h4>
+            <div className="media">
+                <div className="media-left">
+                    <p className="image is-32x32">
+                        <img src="http://bulma.io/images/placeholders/128x128.png" />
+                    </p>
+                </div>
+                <div className="media-content">
+                    <div className="content">
+                        <p>
+                            <a href="#">{c.author}</a> replied 34 minutes ago &nbsp;
+                            <span className="tag">Question</span>
+                        </p>
+                    </div>
+                </div>
+                <div className="media-right">
+                    <span className="has-text-grey-light"><i className="fa fa-comments"></i> 1</span>
+                </div>
+            </div>
+        </article>
+    );
+})
 
 class Forum extends Component {
     render() {
@@ -93,7 +147,7 @@ class Forum extends Component {
                         <div className="column is-9">
                             <div className="box content">
                                 <article className="post">
-                                    <h4>Bulma: How do you center a button in a box?</h4>
+                                    <h4>{comment.message}</h4>
                                     <div className="media">
                                         <div className="media-left">
                                             <p className="image is-32x32">
@@ -103,7 +157,7 @@ class Forum extends Component {
                                         <div className="media-content">
                                             <div className="content">
                                                 <p>
-                                                    <a href="#">@jsmith</a> replied 34 minutes ago &nbsp;
+                                                    <a href="#">{comment.author}</a> replied 34 minutes ago &nbsp;
                                                     <span className="tag">Question</span>
                                                 </p>
                                             </div>
@@ -218,6 +272,7 @@ class Forum extends Component {
                                         </div>
                                     </div>
                                 </article>
+                                {displayCommentList}
                             </div>
                         </div>
                     </div>
